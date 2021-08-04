2fc1068bd753181444956301702bf5c98b23497b
VladPlakhotnik <pifboostz@gmail.com>
2021-08-04 22:03:31 +0200

    add all files at the first time

---
+++ main.js
@@ -0,0 +1,10 @@
+/*let question = prompt("Как тебя зовут ?", "");
+let age = prompt("Сколько тебе лет ?", "");
+if (age >= 18) document.write("Вам есть уже 18");
+else window.close()
+document.write("<br>Добро Пожаловать " + question);*/
+
+let a = prompt("Первое число?", 1);
+let b = prompt("Второе число?", 2);
+
+alert(+a + +b); // 12
